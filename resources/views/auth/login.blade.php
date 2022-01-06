15366884e15ad7d44348cc1bb9702a2021c3e0c4
frankzohim <delanofofe@gmail.com>
2022-01-06 17:43:47 +0200

    commit from monaco

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,13 +21,7 @@
                         </div>
                     </div>
                     <div class="card-body px-lg-5 py-lg-5">
-                        <div class="text-center text-muted mb-4">
-                            <small>
-                                    Create new account OR Sign in with these credentials:
-                                    <br>
-                                    Username <strong>admin@argon.com</strong> Password: <strong>secret</strong>
-                            </small>
-                        </div>
+                       
                         <form role="form" method="POST" action="{{ route('login') }}">
                             @csrf
 
